9468c94d915345b84cd00c3511d8e1097c1bb0ed
fidansamet <fidansamet@hacettepe.edu.tr>
2019-12-20 08:20:43 +0300

    Many things added

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,34 @@ var windowHalfY = window.innerHeight / 2;
 var object;
 var radius = 6371;      // constant scalar for star background
 var i, j;       // loop identifiers
-
+var longTimeAgoText = "A long time ago, in a galaxy far, \nfar away...";
+var gameName = "AGAINST THE\n  DARK SIDE";
+var gameIntrotextArr = ["It is a period of civil war. Rebel",
+                        "spaceships, striking from a hidden",
+                        "base, have won their first victory", 
+                        "against the evil Galactic Empire.", 
+                        "During the battle, Rebel spies managed", 
+                        "to steal secret plans to the Empire's", 
+                        "ultimate weapon, the DEATH STAR an",
+                        "armored space station with enough",
+                        "power to destroy an entire planet.",
+                        "You, as a rebel spy are received",
+                        "the task to transmit the secret plans",
+                        "to hidden base. Each level is continous", 
+                        "to each other. You must fight against",
+                        "the dark side. May the force be with you!"];
+var directionalLight;
+var textMesh;
+var gameNameAnimation = false, introAnimation = false;      // booleans to animate intro texts
+var group;
 
 function init() {
 
     container = document.createElement('div');
     document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1e7);
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1e6);
+    camera.position.z = 100;
     
     // scene
     scene = new THREE.Scene();
@@ -69,7 +88,7 @@ function init() {
     //document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     //window.addEventListener( 'resize', onWindowResize, false );
     
-    createBackgroundWithStars();
+    showStarWarsEntry();
 }
 
 function onWindowResize() {
@@ -93,60 +112,122 @@ function animate() {
 function render() {
     camera.position.x += ( mouseX - camera.position.x ) * .05;
     camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    
+    if (gameNameAnimation) {
+        group.position.z -= 25;
+    } else if (introAnimation) {
+        group.position.z -= 5;
+        console.log(group.position.z);
+    }
+    
     camera.lookAt( scene.position );
     renderer.render( scene, camera );
 }
 
 function showStarWarsEntry () {
-    // instantiate a loader
-    var loader = new THREE.SVGLoader();
+    createBackgroundWithStars ();       // create background with stars
+    
+    // LIGHT
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+    directionalLight.position.set(0, 1, 1);
+    directionalLight.name = 'textLight';
+    scene.add(directionalLight);
+    
+    // TEXT GROUP
+    group = new THREE.Group();      // to work with text as a group
+    group.position.y = 30;
+    scene.add(group);
+    createLongTimeAgoText();
+}
 
-    // load a SVG resource
-    loader.load(
-        // resource URL
-        'datas/star-wars-logo.svg',
-        // called when the resource is loaded
-        function (data) {
+function createLongTimeAgoText () {
+    loadFont(longTimeAgoText, 'fonts/arial.json', 60, 0x64c8c5, 30, -1000, 0);
+    // after 5 seconds remove it add game name
+    setTimeout(function(){
+        createGameNameText();
+    }, 5000);
+}
 
-            var paths = data.paths;
-            var group = new THREE.Group();
-            group.scale.multiplyScalar( 0.25 );
-            group.position.x = - 70;
-            group.position.y = 70;
-            group.scale.y *= - 1;
+function createGameNameText () {
+    group.remove(textMesh);
+    // create an AudioListener and add it to the camera
+    var listener = new THREE.AudioListener();
+    camera.add(listener);
 
-            for (var i = 0; i < paths.length; i ++) {
+    // create a global audio source
+    var sound = new THREE.Audio(listener);
 
-                var path = paths[i];
+    // load a sound and set it as the Audio object's buffer
+    var audioLoader = new THREE.AudioLoader();
+    //audioLoader.load('sounds/star_wars_intro.ogg', function(buffer) {
+//	sound.setBuffer(buffer);
+//	sound.setLoop(false);
+//	sound.setVolume(0.5);
+//	sound.play();
+//    });
+    gameNameAnimation = true;
+    loadFont(gameName, 'fonts/star_wars_entry/logo_font.json', 100, 0xfcdf00, 30, -300, 0);
+    setTimeout(function(){
+        createIntroText();
+    }, 4000);
+}
 
-                var material = new THREE.MeshBasicMaterial( {
-                    color: path.color,
-                    side: THREE.DoubleSide,
-                    depthWrite: false
-                } );
+function createIntroText () {
+    gameNameAnimation = false;
+    introAnimation = true;
+    //group.remove(textMesh);
+    console.log(camera.position.x);
+    console.log(camera.position.y);
+    console.log(camera.position.z);
+    var a = group.position.z;
+    for (i = 0; i < gameIntrotextArr.length; i++) {
+        loadFont(gameIntrotextArr[i], 'fonts/star_wars_entry/intro_font.json', 70, 0xfcdf00, 
+                -window.innerHeight, window.innerHeight / 2 * i - a, -Math.PI / 4);
+    }
+    
+    setTimeout(function(){
+        introAnimation = false;
+        // remove group elements one by one
+        console.log("end intro");
+    }, 30000);
+}
 
-                var shapes = path.toShapes( true );
+function loadFont(text, font, textSize, colorHex, y, z, rotationX) {
+    var loader = new THREE.FontLoader();
+    loader.load(font, function (response) {
+            createText(text, response, textSize, colorHex, y, z, rotationX);
+    } );
+}
 
-                for ( var j = 0; j < shapes.length; j ++ ) {
-                    var shape = shapes[ j ];
-                    var geometry = new THREE.ShapeBufferGeometry( shape );
-                    var mesh = new THREE.Mesh( geometry, material );
-                    group.add( mesh );
-                }
-            }
-            scene.add(group);
-        },
-        
-        // called when loading is in progresses
-        function (xhr) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        
-        // called when loading has errors
-        function ( error ) {
-            console.log( 'An error happened' );
-        }
-    );
+function createText(text, font, textSize, colorHex, y, z, rotationX) {
+    directionalLight.color.setHex(colorHex);
+    materials = [
+            new THREE.MeshPhongMaterial( { color: colorHex, flatShading: true } ), // front
+            new THREE.MeshPhongMaterial( { color: colorHex } ) // side
+    ];
+    
+    textGeo = new THREE.TextBufferGeometry(text, {
+            font: font,
+            size: textSize,
+            height: 5,
+            curveSegments: 12,
+            bevelThickness: 5,
+            bevelSize: 2,
+            bevelEnabled: true
+    } );
+    textGeo.computeBoundingBox();
+    textGeo.computeVertexNormals();
+    // "fix" side normals by removing z-component of normals for side faces
+    // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
+    var centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+    //textGeo = new THREE.BufferGeometry().fromGeometry(textGeo);
+    textMesh = new THREE.Mesh(textGeo, materials);
+    textMesh.position.x = centerOffset;
+    textMesh.position.y = y;
+    textMesh.position.z = z;
+    textMesh.rotation.x = rotationX;
+    textMesh.rotation.y = Math.PI * 2;
+    group.add(textMesh);
 }
 
 // This function adds stars with random position, color and sizes to scene
