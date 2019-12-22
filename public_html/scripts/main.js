c8dde85ac9d933d335a0c5916ab0a5088d05f4d1
fidansamet <fidansamet@hacettepe.edu.tr>
2019-12-22 10:45:43 +0300

    some fixes

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -53,8 +53,6 @@ function init() {
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1e6);
     camera.position.z = 100;
     
-    
-    
     // scene
     scene = new THREE.Scene();
     //var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
@@ -119,11 +117,7 @@ function init() {
     });
     
     window.addEventListener( 'resize', onWindowResize, false );
-    
-
 }
-
-
 
 function onWindowResize() {
     windowHalfX = window.innerWidth / 2;
@@ -136,12 +130,9 @@ function onWindowResize() {
     }
 }
 
-
-
 function animate() {
     requestAnimationFrame(animate);
     render();
-
 }
 
 function render() {
@@ -149,7 +140,7 @@ function render() {
     //camera.position.y += ( - mouseY - camera.position.y ) * .05;
     
     if (gameNameAnimation) {
-        //group.position.z -= 25;
+        group.position.z -= 25;
     } else if (introAnimation) {
         group.position.z -= 5;
         console.log(group.position.z);
@@ -161,7 +152,6 @@ function render() {
     if (startTerrain && controls !== undefined) {
         controls.update( clock.getDelta() );
     }
-
 }
 
 function showStarWarsEntry () {
@@ -358,6 +348,7 @@ export function createLevelMap () {
 }
 
 function createGameScene() {
+    onLevelMap = false; // these needs to be checked later
     directionalLight.color.setHex(0xffffff);
     createTerrain();
     createSky();
@@ -365,22 +356,17 @@ function createGameScene() {
 }
 
 function createTerrain() {
-    onLevelMap = false; // these needs to be checked later
     startTerrain = true;
-    animate();
-
-    container = document.getElementById( 'container' );
-
     scene.background = new THREE.Color( 0xbfd1e5 );
-    var data = generateHeight( worldWidth, worldDepth );
+    var data = generateTerrainHeight( worldWidth, worldDepth );
     camera.position.y = data[ worldHalfWidth + worldHalfDepth * worldWidth ] * 10 + 500;
     var geometry = new THREE.PlaneBufferGeometry( 7500, 7500, worldWidth - 1, worldDepth - 1 );
     geometry.rotateX( - Math.PI / 2 );
     var vertices = geometry.attributes.position.array;
     for ( var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
-            vertices[ j + 1 ] = data[ i ] * 10;
+        vertices[ j + 1 ] = data[ i ] * 10;
     }
-    texture = new THREE.CanvasTexture( generateTexture( data, worldWidth, worldDepth ) );
+    texture = new THREE.CanvasTexture( generateTerrainTexture( data, worldWidth, worldDepth ) );
     texture.wrapS = THREE.ClampToEdgeWrapping;
     texture.wrapT = THREE.ClampToEdgeWrapping;
     mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
@@ -389,62 +375,61 @@ function createTerrain() {
     controls = new FirstPersonControls( camera, renderer.domElement );
     controls.movementSpeed = 1000;
     controls.lookSpeed = 0.1;
-
 }
 
-function generateHeight( width, height ) {
-        var size = width * height, data = new Uint8Array( size ),
-                perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
-        for ( var j = 0; j < 4; j ++ ) {
-                for ( var i = 0; i < size; i ++ ) {
-                        var x = i % width, y = ~ ~ ( i / width );
-                        data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
-                }
-                quality *= 5;
+function generateTerrainHeight( width, height ) {
+    var size = width * height, data = new Uint8Array( size ),
+        perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
+    for ( var j = 0; j < 4; j ++ ) {
+        for ( var i = 0; i < size; i ++ ) {
+            var x = i % width, y = ~ ~ ( i / width );
+            data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
         }
-        return data;
+        quality *= 5;
+    }
+    return data;
 }
-function generateTexture( data, width, height ) {
-        var canvas, canvasScaled, context, image, imageData, vector3, sun, shade;
-        vector3 = new THREE.Vector3( 0, 0, 0 );
-        sun = new THREE.Vector3( 1, 1, 1 );
-        sun.normalize();
-        canvas = document.createElement( 'canvas' );
-        canvas.width = width;
-        canvas.height = height;
-        context = canvas.getContext( '2d' );
-        context.fillStyle = '#000';
-        context.fillRect( 0, 0, width, height );
-        image = context.getImageData( 0, 0, canvas.width, canvas.height );
-        imageData = image.data;
-        for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
-                vector3.x = data[ j - 2 ] - data[ j + 2 ];
-                vector3.y = 2;
-                vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
-                vector3.normalize();
-                shade = vector3.dot( sun );
-                imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
-                imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-                imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-        }
-        context.putImageData( image, 0, 0 );
-        // Scaled 4x
-        canvasScaled = document.createElement( 'canvas' );
-        canvasScaled.width = width * 4;
-        canvasScaled.height = height * 4;
-        context = canvasScaled.getContext( '2d' );
-        context.scale( 4, 4 );
-        context.drawImage( canvas, 0, 0 );
-        image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
-        imageData = image.data;
-        for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
-                var v = ~ ~ ( Math.random() * 5 );
-                imageData[ i ] += v;
-                imageData[ i + 1 ] += v;
-                imageData[ i + 2 ] += v;
-        }
-        context.putImageData( image, 0, 0 );
-        return canvasScaled;
+function generateTerrainTexture( data, width, height ) {
+    var canvas, canvasScaled, context, image, imageData, vector3, sun, shade;
+    vector3 = new THREE.Vector3( 0, 0, 0 );
+    sun = new THREE.Vector3( 1, 1, 1 );
+    sun.normalize();
+    canvas = document.createElement( 'canvas' );
+    canvas.width = width;
+    canvas.height = height;
+    context = canvas.getContext( '2d' );
+    context.fillStyle = '#000';
+    context.fillRect( 0, 0, width, height );
+    image = context.getImageData( 0, 0, canvas.width, canvas.height );
+    imageData = image.data;
+    for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+        vector3.x = data[ j - 2 ] - data[ j + 2 ];
+        vector3.y = 2;
+        vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
+        vector3.normalize();
+        shade = vector3.dot( sun );
+        imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+    }
+    context.putImageData( image, 0, 0 );
+    // Scaled 4x
+    canvasScaled = document.createElement( 'canvas' );
+    canvasScaled.width = width * 4;
+    canvasScaled.height = height * 4;
+    context = canvasScaled.getContext( '2d' );
+    context.scale( 4, 4 );
+    context.drawImage( canvas, 0, 0 );
+    image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+    imageData = image.data;
+    for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
+        var v = ~ ~ ( Math.random() * 5 );
+        imageData[ i ] += v;
+        imageData[ i + 1 ] += v;
+        imageData[ i + 2 ] += v;
+    }
+    context.putImageData( image, 0, 0 );
+    return canvasScaled;
 }
 
 function createSky () {
