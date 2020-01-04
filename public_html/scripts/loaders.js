0b2e7af554b25c535f750b897c411716ae871f61
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-01-04 16:40:00 +0300

    landspeeder locked to camera

--- public_html/scripts/loaders.js
+++ public_html/scripts/loaders.js
@@ -1,29 +1,28 @@
 import { GLTFLoader } from '../three.js-dev/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from '../three.js-dev/examples/jsm/loaders/DRACOLoader.js';
 
-export function gltfLoad(path, scene, camera) {
+export async function gltfLoad(path, scene, camera) {
     // Instantiate a loader
     var loader = new GLTFLoader();
-
     // Optional: Provide a DRACOLoader instance to decode compressed mesh data
     var dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath( '../three.js-dev/examples/js/libs/draco/' );
     loader.setDRACOLoader( dracoLoader );
 
     // Load a glTF resource
-    loader.load(
+    await loader.load(
             // resource URL
             path,
             // called when the resource is loaded
             function ( gltf ) {
 
+                    gltf.scene.scale.set(100, 100, 100); // THREE.Scene
+                    gltf.scene.rotation.set(0,Math.PI,0);
+                    gltf.scene.name = "OSG_Scene" ;
                     scene.add( gltf.scene );
 
-                    gltf.scene.scale.set(100, 100, 100); // THREE.Scene
                     
-                    gltf.scene.position.x = camera.position.x;
-                    gltf.scene.position.y = camera.position.y;
-                    gltf.scene.position.z = camera.position.z;
+                    
                     
 
             },
