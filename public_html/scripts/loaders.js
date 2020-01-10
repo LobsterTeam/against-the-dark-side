63e6ce12fc04844376ea6bd997957b0e1a2b6cea
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-01-10 10:19:08 +0300

    animated model loader added(gltf)

--- public_html/scripts/loaders.js
+++ public_html/scripts/loaders.js
@@ -4,6 +4,9 @@ import { MTLLoader } from '../three.js-dev/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from '../three.js-dev/examples/jsm/loaders/OBJLoader.js';
 import { FBXLoader } from '../three.js-dev/examples/jsm/loaders/FBXLoader.js';
 import { TGALoader } from '../three.js-dev/examples/jsm/loaders/TGALoader.js';
+import { AnimationMixer } from '../three.js-dev/src/animation/AnimationMixer.js';
+
+export var mixer;
 
 export async function gltfLoad(manager, path, scene, camera, objName, x, y, z, scale, yRotation) {
     // Instantiate a loader
@@ -24,7 +27,7 @@ export async function gltfLoad(manager, path, scene, camera, objName, x, y, z, s
                     gltf.scene.rotation.set(0, yRotation, 0);
                     gltf.scene.name = objName ;
                     gltf.scene.position.set(x, y, z);
-                    scene.add( gltf.scene );
+                    //scene.add( gltf.scene );
             },
             // called while loading is progressing
             function ( xhr ) {
@@ -35,6 +38,49 @@ export async function gltfLoad(manager, path, scene, camera, objName, x, y, z, s
             // called when loading has errors
             function ( error ) {
 
+                    console.log( 'An error happened' );
+
+            }
+    );
+}
+
+export async function animatedGltfLoad(manager, path, scene, camera, objName, x, y, z, scale, yRotation) {
+    // Instantiate a loader
+    var loader = new GLTFLoader(manager);
+    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+    var dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( '../three.js-dev/examples/js/libs/draco/' );
+    loader.setDRACOLoader( dracoLoader );
+
+    // Load a glTF resource
+    await loader.load(
+            // resource URL
+            path,
+            // called when the resource is loaded
+            function ( gltf ) {
+
+                    mixer = new AnimationMixer( gltf.scene );
+                    console.log(gltf);
+                    var action = mixer.clipAction( gltf.animations[ 0 ] );
+                    action.play();
+
+ 
+                    gltf.scene.scale.set(scale, scale, scale); // THREE.Scene
+                    gltf.scene.rotation.set(0, yRotation, 0);
+                    gltf.scene.name = objName ;
+                    gltf.scene.position.set(x, y, z);
+                    scene.add( gltf.scene );
+
+            },
+            // called while loading is progressing
+            function ( xhr ) {
+
+                    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+            },
+            // called when loading has errors
+            function ( error ) {
+                    console.log(error);
                     console.log( 'An error happened' );
 
             }
@@ -55,7 +101,7 @@ export async function objLoad (manager, mtlPath, objPath, scene, camera, objName
             obj.scale.set(scale, scale, scale);
             obj.rotation.y += yRotation;
             obj.castShadow = true;
-            scene.add(obj);
+            //scene.add(obj);
         }, onProgress, onError);
     });
     
@@ -71,16 +117,16 @@ export async function objLoad (manager, mtlPath, objPath, scene, camera, objName
     }
 }
 
-export async function fbxLoad (path, scene, camera, objName, x, y, z, scale, yRotation) {
+export async function fbxLoad (manager, path, scene, camera, objName, x, y, z, scale, yRotation) {
     
-    var loader = new FBXLoader();
+    var loader = new FBXLoader(manager);
     await loader.load( path, function ( object ) {
             mixer = new THREE.AnimationMixer( object );
             var action = mixer.clipAction( object.animations[ 0 ] );
             action.play();
             object.traverse( function ( child ) {
                 if ( child.isMesh ) {
-                        childscale.set(scale, scale, scale);
+                        child.scale.set(scale, scale, scale);
                         child.castShadow = true;
                         child.receiveShadow = true;
                 }
