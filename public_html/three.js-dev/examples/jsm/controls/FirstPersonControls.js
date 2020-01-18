af06cecef86bc8d0920ed75dd7ddea1abdee2bdb
fidansamet <fidansamet@hacettepe.edu.tr>
2020-01-18 20:05:48 +0300

    FirstPersonControls fixed

--- public_html/three.js-dev/examples/jsm/controls/FirstPersonControls.js
+++ public_html/three.js-dev/examples/jsm/controls/FirstPersonControls.js
@@ -5,7 +5,7 @@
  */
 
 import {
-	MathUtils,
+	Math as _Math,
 	Spherical,
 	Vector3
 } from "../../../build/three.module.js";
@@ -235,7 +235,7 @@ var FirstPersonControls = function ( object, domElement ) {
 
 			if ( this.heightSpeed ) {
 
-				var y = MathUtils.clamp( this.object.position.y, this.heightMin, this.heightMax );
+				var y = _Math.clamp( this.object.position.y, this.heightMin, this.heightMax );
 				var heightDelta = y - this.heightMin;
 
 				this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
@@ -278,12 +278,12 @@ var FirstPersonControls = function ( object, domElement ) {
 
 			lat = Math.max( - 85, Math.min( 85, lat ) );
 
-			var phi = MathUtils.degToRad( 90 - lat );
-			var theta = MathUtils.degToRad( lon );
+			var phi = _Math.degToRad( 90 - lat );
+			var theta = _Math.degToRad( lon );
 
 			if ( this.constrainVertical ) {
 
-				phi = MathUtils.mapLinear( phi, 0, Math.PI, this.verticalMin, this.verticalMax );
+				phi = _Math.mapLinear( phi, 0, Math.PI, this.verticalMin, this.verticalMax );
 
 			}
 
@@ -346,8 +346,8 @@ var FirstPersonControls = function ( object, domElement ) {
 		lookDirection.set( 0, 0, - 1 ).applyQuaternion( quaternion );
 		spherical.setFromVector3( lookDirection );
 
-		lat = 90 - MathUtils.radToDeg( spherical.phi );
-		lon = MathUtils.radToDeg( spherical.theta );
+		lat = 90 - _Math.radToDeg( spherical.phi );
+		lon = _Math.radToDeg( spherical.theta );
 
 	}
 
